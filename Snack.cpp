ed47d3364ab589d375b53105197720394db8f252
Anthony Seng <aseng6825@csu.fullerton.edu>
2023-10-04 20:35:24 -0700

    Ver 0.2

--- Snack.cpp
+++ Snack.cpp
@@ -7,3 +7,317 @@
 #include <utility>                                                    // move()
 
 #include "Snack.hpp"
+
+// Default and Conversion Constructor
+Snack::Snack( std::string productName, std::string brandName, std::string upcCode, std::string vendingNum, double price )
+  ///////////////////////// TO-DO (2) //////////////////////////////
+  /// Copying the parameters into the object's attributes (member variables) "works" but is not correct.  Be sure to move the parameters into the object's attributes
+  : _upcCode( std::move( upcCode ) ), _brandName( std::move( brandName ) ), _productName( std::move( productName ) ), _vendingNum( std::move( vendingNum ) ), _price( std::move( price ) )
+{}
+/////////////////////// END-TO-DO (2) ////////////////////////////
+
+
+
+
+// Copy constructor
+Snack::Snack( Snack const & other )
+  ///////////////////////// TO-DO (3) //////////////////////////////
+  : _upcCode( other._upcCode ), _brandName( other._brandName ), _productName( other._productName ), _vendingNum( other._vendingNum ), _price( other._price )
+{}
+/////////////////////// END-TO-DO (3) ////////////////////////////
+
+
+
+
+// Move constructor
+Snack::Snack( Snack && other ) noexcept
+  ///////////////////////// TO-DO (4) //////////////////////////////
+  : _upcCode( std::move( other._upcCode ) ), _brandName( std::move( other._brandName ) ), _productName( std::move( other._productName ) ), _vendingNum( std::move( other._vendingNum ) ), _price( std::move( other._price ) )
+{
+}
+/////////////////////// END-TO-DO (4) ////////////////////////////
+
+
+
+
+// Copy Assignment Operator
+Snack & Snack::operator=( Snack const & rhs ) &
+///////////////////////// TO-DO (5) //////////////////////////////
+{
+  if( this != &rhs )
+  {
+    _productName = rhs._productName;
+    _brandName   = rhs._brandName;
+    _upcCode     = rhs._upcCode;
+    _vendingNum     = rhs._vendingNum;
+    _price       = rhs._price;
+  }
+  return *this;
+}
+/////////////////////// END-TO-DO (5) ////////////////////////////
+
+
+
+
+// Move Assignment Operator
+Snack & Snack::operator=( Snack && rhs ) & noexcept
+///////////////////////// TO-DO (6) //////////////////////////////
+{
+  if( this != &rhs )
+  {
+    _productName = std::move( rhs._productName );
+    _brandName   = std::move( rhs._brandName );
+    _upcCode     = std::move( rhs._upcCode );
+    _vendingNum     = std::move( rhs._vendingNum );
+    _price       = std::move( rhs._price );
+  }
+  return *this;
+}
+/////////////////////// END-TO-DO (6) ////////////////////////////
+
+
+
+// Destructor
+Snack::~Snack() noexcept
+///////////////////////// TO-DO (7) //////////////////////////////
+{
+}
+/////////////////////// END-TO-DO (7) ////////////////////////////
+
+
+
+
+
+
+
+
+/*******************************************************************************
+**  Accessors
+*******************************************************************************/
+
+// upcCode() const
+std::string const & Snack::upcCode() const &
+{
+  ///////////////////////// TO-DO (8) //////////////////////////////
+  return _upcCode;
+  /////////////////////// END-TO-DO (8) ////////////////////////////
+}
+
+
+
+
+// brandName() const
+std::string const & Snack::brandName() const &
+{
+  ///////////////////////// TO-DO (9) //////////////////////////////
+  return _brandName;
+  /////////////////////// END-TO-DO (9) ////////////////////////////
+}
+
+
+
+
+// productName() const
+std::string const & Snack::productName() const &
+{
+  ///////////////////////// TO-DO (10) //////////////////////////////
+  return _productName;
+  /////////////////////// END-TO-DO (10) ////////////////////////////
+}
+
+// vendingNum() const
+std::string const & Snack::vendingNum() const &
+{
+  ///////////////////////// TO-DO (10) //////////////////////////////
+  return _vendingNum;
+  /////////////////////// END-TO-DO (10) ////////////////////////////
+}
+
+// price() const
+double Snack::price() const &
+{
+  ///////////////////////// TO-DO (11) //////////////////////////////
+  return _price;
+  /////////////////////// END-TO-DO (11) ////////////////////////////
+}
+
+
+
+
+// upcCode()
+std::string Snack::upcCode() &&
+{
+  ///////////////////////// TO-DO (12) //////////////////////////////
+  return std::move( _upcCode );
+  /////////////////////// END-TO-DO (12) ////////////////////////////
+}
+
+
+
+
+// brandName()
+std::string Snack::brandName() &&
+{
+  ///////////////////////// TO-DO (13) //////////////////////////////
+  return std::move( _brandName );
+  /////////////////////// END-TO-DO (13) ////////////////////////////
+}
+
+
+
+
+// productName()
+std::string Snack::productName() &&
+{
+  ///////////////////////// TO-DO (14) //////////////////////////////
+  return std::move( _productName );
+  /////////////////////// END-TO-DO (14) ////////////////////////////
+}
+
+// vendingNum()
+std::string Snack::vendingNum() &&
+{
+  ///////////////////////// TO-DO (14) //////////////////////////////
+  return std::move( _vendingNum );
+  /////////////////////// END-TO-DO (14) ////////////////////////////
+}
+
+
+
+
+
+
+/*******************************************************************************
+**  Modifiers
+*******************************************************************************/
+
+// upcCode()
+Snack & Snack::upcCode( std::string newUpcCode ) &
+{
+  ///////////////////////// TO-DO (15) //////////////////////////////
+    /// Copy assignment "works" but is not correct.  Be sure to move newUpcCode into _upcCode
+  _upcCode = std::move( newUpcCode );
+  return *this;
+  /////////////////////// END-TO-DO (15) ////////////////////////////
+}
+
+
+
+
+// brandName()
+Snack & Snack::brandName( std::string newBrandName ) &
+{
+  ///////////////////////// TO-DO (16) //////////////////////////////
+  _brandName = std::move( newBrandName );
+  return *this;
+  /////////////////////// END-TO-DO (16) ////////////////////////////
+}
+
+
+
+
+// productName()
+Snack & Snack::productName( std::string newProductName ) &
+{
+  ///////////////////////// TO-DO (17) //////////////////////////////
+  _productName = std::move( newProductName );
+  return *this;
+  /////////////////////// END-TO-DO (17) ////////////////////////////
+}
+
+// vendingNum()
+Snack & Snack::vendingNum( std::string newVendingNum ) &
+{
+  ///////////////////////// TO-DO (17) //////////////////////////////
+  _vendingNum = std::move( newVendingNum );
+  return *this;
+  /////////////////////// END-TO-DO (17) ////////////////////////////
+}
+
+// price()
+Snack & Snack::price( double newPrice ) &
+{
+  ///////////////////////// TO-DO (18) //////////////////////////////
+  _price = std::move( newPrice );
+  return *this;
+  /////////////////////// END-TO-DO (18) ////////////////////////////
+}
+
+
+
+
+
+// operator==
+bool Snack::operator==( const Snack & rhs ) const noexcept
+{
+  // All attributes must be equal for the two grocery items to be equal to the other.  This can be done in any order, so put the
+  // quickest and then the most likely to be different first.
+
+  ///////////////////////// TO-DO (20) //////////////////////////////
+  return _price == rhs._price && _upcCode == rhs._upcCode && _brandName == rhs._brandName && _vendingNum == rhs._vendingNum && _productName == rhs._productName;
+  /////////////////////// END-TO-DO (20) ////////////////////////////
+}
+
+
+
+
+
+
+
+
+/*******************************************************************************
+**  Insertion and Extraction Operators
+*******************************************************************************/
+
+// operator>>
+std::istream & operator>>( std::istream & stream, Snack & snackItem )
+{
+  ///////////////////////// TO-DO (21) //////////////////////////////
+    /// A lot can go wrong when reading from streams - no permission, wrong types, end of file, etc. Minimal exception guarantee says
+    /// there should be no side affects if an error or exception occurs, so let's do our work in a local object and move it into place
+    /// at the end if all goes well.
+    ///
+    /// This function should be symmetrical with operator<< below.  Read what your write, and write what you read
+    ///
+    ///
+    /// Assume fields are separated by commas & optional spaces, and string attributes are enclosed with double quotes.  For example:
+    ///    UPC Code         | Brand Name | Product Name                                                  | Price
+    ///    -----------------+------------+---------------------------------------------------------------+-------
+    ///    "00034000020706",  "York",      "York Peppermint Patties Dark Chocolate Covered Snack Size"  ,  12.64
+    ///
+    ///
+    /// Hint:  Use std::quoted to read and write quoted strings.  See
+    ///        1) https://en.cppreference.com/w/cpp/io/manip/quoted
+    ///        2) https://www.youtube.com/watch?v=Mu-GUZuU31A
+  Snack temp;
+  char        delimiter = '\0';
+
+
+  if( stream >> std::ws >> std::quoted( temp._upcCode ) && stream >> std::ws >> delimiter && delimiter == ',' && stream >> std::ws >> std::quoted( temp._brandName ) && stream >> std::ws >> delimiter && delimiter == ',' && stream >> std::ws >> std::quoted( temp._productName ) && stream >> std::ws >> delimiter && delimiter == ',' && stream >> std::ws >> std::quoted( temp._vendingNum ) && stream >> std::ws >> delimiter && delimiter == ',' && stream >> std::ws >> temp._price )
+  {
+    snackItem = std::move( temp );
+  }
+  else
+  {
+    stream.setstate( std::ios::failbit );
+  }
+
+
+  return stream;
+  /////////////////////// END-TO-DO (21) ////////////////////////////
+}
+
+
+
+
+// operator<<
+std::ostream & operator<<( std::ostream & stream, const Snack & Snack )
+{
+  ///////////////////////// TO-DO (22) //////////////////////////////
+    /// This function should be symmetrical with operator>> above.  Read what your write, and write what you read
+    ///
+    /// Hint:  Brand and product names may have quotes, which need to escaped when printing.  Use std::quoted to read and write quoted strings.  See
+    ///        1) https://en.cppreference.com/w/cpp/io/manip/quoted
+    ///        2) https://www.youtube.com/watch?v=Mu-GUZuU31A
+  return stream << std::quoted( Snack._upcCode ) << ", " << std::quoted( Snack._brandName ) << ", " << std::quoted( Snack._productName ) << ", " << std::quoted( Snack._vendingNum ) << ", "
+                << Snack._price;
--- Snack.hpp
+++ Snack.hpp
@@ -1,3 +1,4 @@
+#include <compare>                                                            // std::weak_ordering
 #include <iostream>
 #include <string>
 
@@ -11,7 +12,8 @@ class Snack
     // Constructors, assignments, and destructor
     Snack( std::string productName = {},                                // Default and Conversion (from string to GroceryItem) constructor
                  std::string brandName   = {},                                // String parameters intentionally passed by value.  Not perfect, but very very
-                 std::string upcCode     = {},                                // good when combined with move semantics. 
+                 std::string upcCode     = {},
+                 std::string vendingNum   = {},                                // good when combined with move semantics. 
                  double      price       = 0.0 );
 
     Snack & operator=( Snack const  & rhs   ) &;                  // Assignment operators available only for l-values (that's what the trailing "&" means), and then
@@ -24,28 +26,29 @@ class Snack
     // Accessors
     std::string const & upcCode    () const &;                                // Returns object's state by constant reference for l-value objects and by value for r-value objects
     std::string const & brandName  () const &;                                // The "const &" at the end says these functions will be called for l-value objects and r-value objects
-    std::string const & productName() const &;                                // that (listen carefully) haven't been overloaded.
+    std::string const & productName() const &;
+    std::string const & vendingNum() const &;                                // that (listen carefully) haven't been overloaded.
     double              price      () const &;                                //
                                                                               //
     std::string         upcCode    ()       &&;                               // Overloads that return an r-value object's state by value (unsafe to return an r-value's state by reference)
     std::string         brandName  ()       &&;                               // The "&&" at the end says these functions will be called only for r-value objects
-    std::string         productName()       &&;                               
+    std::string         productName()       &&;
+    std::string         vendingNum()       &&;                               
                                                                               
 
     // Modifiers                                                              // Updates object's state and returns a reference to self (enables chaining)
     Snack & upcCode    ( std::string newUpcCode     ) &;                // String parameters intentionally passed by value
     Snack & brandName  ( std::string newBrandName   ) &;                // Modifiers available for l-values only         (The & at the end says these functions will be called only for l-values)
-    Snack & productName( std::string newProductName ) &;                // OK:     GroceryItem b; b.price(13.99);        (b is an l-value, i.e. a named object)
+    Snack & productName( std::string newProductName ) &;
+    Snack & vendingNum    ( std::string newVendingNum     ) &;                // OK:     GroceryItem b; b.price(13.99);        (b is an l-value, i.e. a named object)
     Snack & price      ( double      newPrice       ) &;                // Error:  GroceryItem{}.price(13.99);           (The default constructed GrocerItem is an r-value, i.e., an unnamed temporary object)
 
-
-    // Relational Operators
-    std::weak_ordering operator<=>( Snack const & rhs ) const noexcept;
     bool               operator== ( Snack const & rhs ) const noexcept;
 
   private:
     std::string _upcCode;                                                     // a 12 or 14-digit international Universal Product Code uniquely identifying this item (Ex: 051600080015, 05017402006207)
     std::string _brandName;                                                   // the product manufacturer's brand name (Ex: Heinz, Boston Market)
-    std::string _productName;                                                 // the name of the product (Ex: Heinz Tomato Ketchup - 2 Ct, Boston Market Spaghetti With Meatballs)
+    std::string _productName;
+    std::string _vendingNum;                                                 // the name of the product (Ex: Heinz Tomato Ketchup - 2 Ct, Boston Market Spaghetti With Meatballs)
     double      _price = 0.0;                                                 // the cost of the item in US Dollars (Ex:  2.29, 1.19)
 };
