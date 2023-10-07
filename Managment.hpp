2360ae39609c52fa29e3eacdcf5fdbd157df005e
John Bither <john.bither@csu.fullerton.edu>
2023-10-06 21:34:22 -0700

    Made Managment class hpp in line with class dia

---
+++ Managment.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "Snack.hpp"    //for now as that is the only one i saw on github
+
+class Managment {
+    public:
+    std::string ManagmentName;
+    bool Recall(Snack object);
+    private:
+    //void ReportData(inventory);
+    // void operation2(params);             not sure what these for
+    //void Operation();                     
+};
