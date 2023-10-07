4297ed79034cf126bc8c169530528524c1e72782
John Bither <john.bither@csu.fullerton.edu>
2023-10-06 20:03:12 -0700

    just adding the initial draft of the ClassManagment

---
+++ ClassManagment.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+class Managment {
+    public:
+    Managment();
+    void displayPrice(std::string input);       // for when customer enter display #
+    bool sale (int payment);                    // for when customer inserts money to buy stuff
+    void restocking();                          // for when Restocker needs to restock
+    bool turnOff(std::string input);            // to shut off machine
+
+// I really don't know exactly what this was supposed to do so i just put these 
+
+
+};
