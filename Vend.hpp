a300a1944e2fda136fd67bc7a6b9bd1cd6ee697b
jmcross02 <143852734+jmcross02@users.noreply.github.com>
2023-10-20 20:38:49 -0700

    Add files via upload

--- Vend.hpp
+++ Vend.hpp
@@ -1,6 +1,8 @@
-#pragma once
+/*#pragma once
 #include <iostream>
 #include <string>
+#include "Management.hpp"
+#include "Vend.cpp"
 
 using namespace std;
 
@@ -18,29 +20,42 @@ class Vend
 	}
 	void Customer_Request()
 	{
-		cout << "Welcome! Please Enter Your Snack Request: ";
-		cin >> RequestNum;
-		if (RequestNum == (ValidInput))
-		{
-			cout << "Vending snack!" << endl;
-			VendItem(/*RequestNum*/);
-		}
-		else
-		{
-			cout << "This item is either out of stock, expired, or has been recalled" << endl;
-		}
 
+	}
+	void CheckRecall()
+	{
 
-			
 	}
 	void VendItem()
 	{
-		Sales = Sales + 4; /*REPLACE WITH PRICE VARIABLE FROM INVENTORY*/
-		snackTracker = snackTracker + 1;
+
 	}
 	void Notification() //sends a notification to management if out of stock or expired items are being requested.
 	{
 
 	}
 
+}; */
+
+#pragma once
+#include <iostream>
+#include <string>
+
+class Vend
+{
+public:
+	Vend(); // Constructor
+	void Customer_Request();
+	void VendItem();
+	void Notification();
+
+private:
+	char RequestNum; // the number the customer inputs to choose a snack.
+	double MoneyIn; // tracks how much total money is in the machine
+	double Sales = 0; // tracks the amount of money made from sales
+	int snackTracker; // tracks the number of sold items
+	char ValidInput;
+
+	void checkInventory();
+	void checkRecall();
 };
