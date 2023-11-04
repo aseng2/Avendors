72f50d86806dd1f228117b9f722627138febfba5
AlSamayoa <92232622+AlSamayoa@users.noreply.github.com>
2023-11-04 02:51:07 -0700

    Update Management.hpp

--- Management.hpp
+++ Management.hpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #pragma once
 #include "Snack.hpp"    //for now as that is the only one i saw on github
 
 class Management {
     public:
-    std::string const ManagementName();
+    Management(std::string name, std::vector<std::string> recall);
     bool Recall(Snack object);
+    void setName(std::string newName);
+    void setRecall(std::vector<std::string> newRecall);
+    std::string getName();
     
     private:
-    //void ReportData(inventory);
+    std:vector<std::string> _recall;
+    std::string _name;
     // void operation2(params);             not sure what these for
     //void Operation();                     
 };
