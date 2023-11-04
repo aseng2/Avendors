d9b58a9f3098321611db4a97f07a5f5024fd4bef
AlSamayoa <92232622+AlSamayoa@users.noreply.github.com>
2023-11-04 03:10:07 -0700

    Update Management.cpp

--- Management.cpp
+++ Management.cpp
@@ -1,13 +1,30 @@
 #include "Management.hpp"
 
-std::string const Management::ManagementName() {
-    return "Avendors";
-};
-
-bool Management::Recall(Snack object, vector<std::string> v)
+Management::Management(std::string name, std::vector<std::string> recall){
+    _name = name;
+    _recall.clear();
+    for(std::string i : recall)
+        _recall.push_back(i);
+}
+Management::Management(){
+    _name = "";
+    _recall.clear();
+}
+void Management::setName(std::string newName){
+    _name = newName;
+    }
+std::string Management::getName(){
+    return _name;
+}
+void Management::setRecall(std::vector<std::string> newList){
+    _recall.clear();
+    for(std::string i : newList)
+        _recall.push_back(i);
+    }
+bool Management::Recall(Snack object)
 {
-    //check the vector v
-    for(std::string i : v){
+    //check if item is in recall
+    for(std::string i : _recall){
         if (i == object.UPC){
             return true;
         }
