51094ee25cb9b9bbcbb1445fbd7af82c6a3302bb
AlSamayoa <92232622+AlSamayoa@users.noreply.github.com>
2023-11-04 10:53:49 -0700

    Update Managementtest.cpp

--- Managementtest.cpp
+++ Managementtest.cpp
@@ -24,14 +24,14 @@ void readFile(std::string title){
 
 int main() {
     readFile("recall.txt");
-    Snack tester("UPC code - 132156", "Dorritos", "nacho", "position - A1", 10/*quantity*/, 5.99/*price*/);
+    Snack tester("132156", "Dorritos", "nacho", "position - A1", 10/*quantity*/, 5.99/*price*/);
     Management test("Avendor", recall_items);
     std::string result = (test.Recall(tester))? "Recall - expected result" : "No recall - error";
     std::cout << result << std::endl;
-    Snack Safe ("UPC code - 132156", "Cheetos", "cheesy", "position - A1", 10/*quantity*/, 5.99/*price*/);
+    Snack Safe ("132156", "Cheetos", "cheesy", "position - A1", 10/*quantity*/, 5.99/*price*/);
     result = (test.Recall(Safe))? "Recall - error" : "No recall - expected result";
     std::cout << result << std::endl;
     std::cout << "Management Name: " << test.getName() << std::endl;
-    std::cout << "Items in recall: << test.display();
+    std::cout << "Items UPC's in recall: << test.display();
     return 1;
 }
