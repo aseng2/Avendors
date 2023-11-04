c75133f50d14eb2981170cc444888bdec76b9446
AlSamayoa <92232622+AlSamayoa@users.noreply.github.com>
2023-11-04 02:23:13 -0700

    Update Managementtest.cpp
    Add function to read from file recall items

--- Managementtest.cpp
+++ Managementtest.cpp
@@ -1,21 +1,43 @@
 #include "Snack.hpp"
 #include "Management.hpp"
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+std::vector<std::string> recall_items;
+
+void readFile(std::string title){
+    string myText;
+    //Read from the text file
+    fstream MyReadFile(title);
+    if (MyReadFile.is_open()){
+        while (getline (MyReadFile, myText)) {
+            recall_items.pushback(myText);
+            }
+        //Close the file
+        MyReadFile.close();
+        }
+        else{
+            cout << "Error opening file";
+            }
+    }
 
 int main() {
-    Snack tester ("UPC code - 132156", "Dorritos", "nacho", "position - A1", 10/*quantity*/, 5.99/*price*/);
+    readFile("recall.txt");
+    Snack tester("UPC code - 132156", "Dorritos", "nacho", "position - A1", 10/*quantity*/, 5.99/*price*/);
     Management test;
     if (test.Recall(tester)){
-        std::cout << "No recall - error" << std::endl;
+        std::cout << "Recall - expected result" << std::endl;
     }
     else {
-        std::cout << "Recall - expected result" << std::endl;
+        std::cout << "No recall - error" << std::endl;
     }
     Snack Safe ("UPC code - 132156", "Cheetos", "cheesy", "position - A1", 10/*quantity*/, 5.99/*price*/);
     if (test.Recall(Safe)){
-        std::cout << "No recall - expected result" << std::endl;
+        std::cout << "Recall - error" << std::endl;        
     }
     else {
-        std::cout << "Recall - error" << std::endl;
+        std::cout << "No recall - expected result" << std::endl;
     }
         std::cout << "Management Name: " << test.ManagementName() << std::endl;
     return 1;
