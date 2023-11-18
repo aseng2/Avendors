358657ecb545b7d02c531521a8b107abdfd6b486
AlSamayoa <92232622+AlSamayoa@users.noreply.github.com>
2023-11-18 07:52:11 -0800

    Update Management.hpp

--- Management.hpp
+++ Management.hpp
@@ -6,15 +6,15 @@
 
 class Management {
     public:
-    Management(std::string name, std::vector<std::string> recall);
+    Management(std::string name, std::vector<Snack> recall);
     bool Recall(Snack object);
     void setName(std::string newName);
-    void setRecall(std::vector<std::string> newRecall);
+    void setRecall(std::vector<Snack> newRecall);
     std::string getName();
     void display();
     
     private:
-    std::vector<std::string> _recall;
+    std::vector<Snack> _recall;
     std::string _name;
     // void operation2(params);             not sure what these for
     //void Operation();                     
